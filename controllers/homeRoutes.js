dc655d3129f8afb5715620aa5adad08320f5c53e
Dexter Dick <@dexterdick@hotmail.com>
2023-01-29 10:59:52 -0500

    added tax-resources-page / added container class - to control layout on large screens and made chages to css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,59 +1,59 @@
-const router = require('express').Router();
-const { TaxReport, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { TaxReport, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (request, response) => {
+router.get("/", async (request, response) => {
     try {
         // Get all tax reports and JOIN with user data
         const taxReportData = await TaxReport.findAll({
             include: [
                 {
                     model: User,
-                    attributes: ['name'],
+                    attributes: ["name"],
                 },
             ],
         });
 
         // Serialize data so the template can read it
-        const taxReports = taxReportData.map((taxReport) => taxReport.get({ plain: true }));
+        const taxReports = taxReportData.map((taxReport) =>
+            taxReport.get({ plain: true })
+        );
 
         // Pass serialized data and session flag into template
-        response.render('homepage', {
+        response.render("homepage", {
             taxReports,
-            logged_in: request.session.logged_in
+            logged_in: request.session.logged_in,
         });
     } catch (error) {
         response.status(500).json(error);
     }
 });
 
-router.get('/taxInputs', async (request, response) => {
+router.get("/taxInputs", async (request, response) => {
     try {
         // Pass serialized data and session flag into template
-        response.render('taxInputs', {
-
-        });
+        response.render("taxInputs", {});
     } catch (error) {
         response.status(500).json(error);
     }
 });
 
-router.get('/taxReport/:id', async (request, response) => {
+router.get("/taxReport/:id", async (request, response) => {
     try {
         const taxReportData = await TaxReport.findByPk(request.params.id, {
             include: [
                 {
                     model: User,
-                    attributes: ['name'],
+                    attributes: ["name"],
                 },
             ],
         });
 
         const taxReport = taxReportData.get({ plain: true });
 
-        response.render('taxReport', {
+        response.render("taxReport", {
             ...taxReport,
-            logged_in: request.session.logged_in
+            logged_in: request.session.logged_in,
         });
     } catch (error) {
         response.status(500).json(error);
@@ -61,54 +61,62 @@ router.get('/taxReport/:id', async (request, response) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/account', withAuth, async (request, response) => {
+router.get("/account", withAuth, async (request, response) => {
     try {
         // Find the logged in user based on the session ID
         const userData = await User.findByPk(request.session.user_id, {
-            attributes: { exclude: ['password'] },
+            attributes: { exclude: ["password"] },
             include: [{ model: TaxReport }],
         });
 
         const user = userData.get({ plain: true });
 
-        response.render('account', {
+        response.render("account", {
             ...user,
-            logged_in: true
+            logged_in: true,
         });
     } catch (error) {
         response.status(500).json(error);
     }
 });
 
-router.get('/login', (request, response) => {
+router.get("/login", (request, response) => {
     // If the user is already logged in, redirect the request to another route
     if (request.session.logged_in) {
-        response.redirect('/account');
+        response.redirect("/account");
         return;
     }
 
-    response.render('login');
+    response.render("login");
 });
 
-router.get('/accountCreation', (request, response) => {
-    response.render('accountCreation');
+router.get("/accountCreation", (request, response) => {
+    response.render("accountCreation");
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/taxOutputs', withAuth, async (request, response) => {
+router.get("/taxOutputs", withAuth, async (request, response) => {
     try {
         // Find the logged in user based on the session ID
         const userData = await User.findByPk(request.session.user_id, {
-            attributes: { exclude: ['password'] },
+            attributes: { exclude: ["password"] },
             include: [{ model: TaxReport }],
         });
 
         const user = userData.get({ plain: true });
 
-        response.render('taxOutputs', {
+        response.render("taxOutputs", {
             ...user,
-            logged_in: true
+            logged_in: true,
         });
+    } catch (error) {
+        response.status(500).json(error);
+    }
+});
+
+router.get("/resources", async (request, response) => {
+    try {
+        response.render("resources");
     } catch (error) {
         response.status(500).json(error);
     }
