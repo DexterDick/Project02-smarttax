75f216a09c9cd6815269d3de23ce3246ac75cccb
John Jamieson <jamieson.john@gmail.com>
2023-01-29 16:00:30 -0500

    Added federal tax

--- public/js/taxInputs.js
+++ public/js/taxInputs.js
@@ -1,8 +1,8 @@
 const taxInputFormHandler = async (event) => {
     event.preventDefault();
 
-    const year = document.querySelector('#inputYear').value.trim();
-    const income = document.querySelector('#inputIncome').value.trim();
+    const year = $('#inputYear').val();
+    const income = $('#inputIncome').val();
 
     if (year && income) {
         const response = await fetch(`/api/taxReports`, {
@@ -24,18 +24,42 @@ const taxInputFormHandler = async (event) => {
 const calculate = async (event) => {
     event.preventDefault();
 
-    const year = document.querySelector('#inputYear').value.trim();
-    const income = document.querySelector('#inputIncome').value.trim();
+    let fedIncomeTax = 0;
 
-    document.querySelector('#outputYear').value = year;
-    document.querySelector('#outputIncome').value = income;
+    const fedBrackets = [0,14398,50197,100392,155625,221708]
+    const fedRates = [0,.15,.205,.26,.29,.33]
+
+    const year = $('#inputYear').val();
+    const income = $('#inputIncome').val();
+
+    if (income < fedBrackets[1]) {
+        // No tax
+    } else if (income < fedBrackets[2]) {
+        fedIncomeTax = (income - fedBrackets[1]) * fedRates[1];
+    } else if (income < fedBrackets[3]) {
+        fedIncomeTax = (fedBrackets[2] - fedBrackets[1]) * fedRates[1];
+        fedIncomeTax = fedIncomeTax + (income - fedBrackets[2]) * fedRates[2];
+    } else if (income < fedBrackets[4]) {
+        fedIncomeTax = (fedBrackets[2] - fedBrackets[1]) * fedRates[1];
+        fedIncomeTax = fedIncomeTax + (fedBrackets[3] - fedBrackets[2]) * fedRates[2];
+        fedIncomeTax = fedIncomeTax + (income - fedBrackets[3]) * fedRates[3];
+    } else if (income < fedBrackets[5]) {
+        fedIncomeTax = (fedBrackets[2] - fedBrackets[1]) * fedRates[1];
+        fedIncomeTax = fedIncomeTax + (fedBrackets[3] - fedBrackets[2]) * fedRates[2];
+        fedIncomeTax = fedIncomeTax + (fedBrackets[4] - fedBrackets[3]) * fedRates[3];
+        fedIncomeTax = fedIncomeTax + (income - fedBrackets[4]) * fedRates[4];
+    } else {
+        fedIncomeTax = (fedBrackets[2] - fedBrackets[1]) * fedRates[1];
+        fedIncomeTax = fedIncomeTax + (fedBrackets[3] - fedBrackets[2]) * fedRates[2];
+        fedIncomeTax = fedIncomeTax + (fedBrackets[4] - fedBrackets[3]) * fedRates[3];
+        fedIncomeTax = fedIncomeTax + (fedBrackets[5] - fedBrackets[4]) * fedRates[4];
+        fedIncomeTax = fedIncomeTax + (income - fedBrackets[5]) * fedRates[5];
+    }
+
+    $('#yearSlot').text(year);
+    $('#taxSlot').text(fedIncomeTax);
 
 };
 
-document
-    .querySelector('.new-taxReport-form')
-    .addEventListener('submit', taxInputFormHandler);
+$('.new-taxReport-form').submit(calculate);
 
-document
-    .querySelector('#submit')
-    .addEventListener('click', calculate);
